c89243ceb1c7d99634d71ff870e0fa9075dfb609
DziedzicFilip <dziedzicfilip00@gmail.com>
2025-02-21 14:43:27 +0100

    responsywnosc na tel v1

--- sites/Nadgodziny.php
+++ sites/Nadgodziny.php
@@ -61,17 +61,16 @@ if (!isset($_SESSION['user_id'])) {
 <div class="content">
     <h2 class="text-center">Nadgodziny</h2>
     <div class="d-flex justify-content-center my-3 flex-wrap">
-    <form method="get" action="Nadgodziny.php" class="d-flex">
-    <?php if(isAdmin()) echo '<input type="text" name="searchUser" placeholder="Search User">'; ?>
-    <input type="date" name="startDate">
-    <input type="date" name="endDate">
-    <button type="submit">Search</button>
-    <button type="submit" name="generatePDF" value="1">Generate PDF</button>
-    </form>
-   
-</div>
+        <form method="get" action="Nadgodziny.php" class="d-flex flex-wrap">
+            <?php if(isAdmin()) echo '<input type="text" name="searchUser" placeholder="Search User" value="' . htmlspecialchars($searchTerm) . '" class="form-control mb-2 me-2">'; ?>
+            <input type="date" name="startDate" value="<?php echo htmlspecialchars($startDate); ?>" class="form-control mb-2 me-2">
+            <input type="date" name="endDate" value="<?php echo htmlspecialchars($endDate); ?>" class="form-control mb-2 me-2">
+            <button type="submit" class="btn btn-primary mb-2 me-2">Search</button>
+            <button type="submit" name="generatePDF" value="1" class="btn btn-secondary mb-2">Generate PDF</button>
+        </form>
+    </div>
     <div id="overtimeContainer" class="row">
-        <?php displayOvertimeCards($searchTerm, $startDate, $endDate); // wyswietalnie kart  ?>
+        <?php displayOvertimeCards($searchTerm, $startDate, $endDate); // wyswietalnie kart ?>
     </div>
 </div>
 <script src="https://cdn.jsdelivr.net/npm/bootstrap@5.3.0/dist/js/bootstrap.bundle.min.js"></script>
